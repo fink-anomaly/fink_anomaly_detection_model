8eea5fae988e67c2b69b9091227b2fd42ef9faed
Timofei Pshenichnyy <timofei.psheno@gmail.com>
2023-09-28 17:26:43 +0300

    check

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='fink_anomaly_detection_model',
-      version='0.3.9',
+      version='0.3.11',
       description='Fink anomaly detection model',
       packages=find_packages(),
       author_email='timofei.psheno@gmail.com',
@@ -19,5 +19,7 @@ setup(name='fink_anomaly_detection_model',
             'fink_ad_model_train = fink_anomaly_detection_model:fink_ad_model_train',
         ],
     },
-    python_requires='>=3.8'
+    python_requires='>=3.8',
+    long_description=readme(),
+    long_description_content_type='text/markdown'
 )
