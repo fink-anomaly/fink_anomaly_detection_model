38641f5d90d0a7532808355bc439a738ef869dc9
Timofei Pshenichnyy <timofei.psheno@gmail.com>
2023-09-28 14:33:22 +0300

    pylint refactoring

--- fink_anomaly_detection_model/main_script_.py
+++ fink_anomaly_detection_model/main_script_.py
@@ -1,36 +1,78 @@
 import pickle
-import sys
 import os.path
 import argparse
 from collections import defaultdict
-from functools import lru_cache, partial
+from functools import lru_cache
+from random import randint, choice
 import pandas as pd
 from tqdm import tqdm
-from random import randint, choice
-from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.ensemble import IsolationForest
 from sklearn.metrics import make_scorer
 from sklearn.model_selection import GridSearchCV
-from sklearn.utils.estimator_checks import check_estimator, check_regressors_train
 from sklearn.metrics import roc_auc_score
 from skl2onnx import to_onnx
 from skl2onnx.common.data_types import FloatTensorType
 
-def extract_one(data, key):
+
+def extract_one(data, key) -> pd.Series:
+    """
+    Function for extracting data from lc_features
+    :param data: dict
+                lc_features dict
+    :param key: str
+                Name of the extracted filter
+    :return: pd.DataFrame
+                Dataframe with a specific filter
+    """
     series = pd.Series(data[key], dtype=float)
     return series
 
-def train_with_forest(data: pd.DataFrame, train_params, scorer_, y_true) -> IsolationForest:
+
+def train_with_forest(data, train_params, scorer_, y_true) -> IsolationForest:
+    """
+    Training of the IsolationForest model
+    :param data: pd.DataFrame
+        Training dataset
+    :param train_params: dict
+        Model hyperparameters
+    :param scorer_: function
+        Model quality evaluation function
+    :param y_true:
+        Target
+    :return: IsolationForest
+        Trained model
+    """
     forest = IsolationForest()
     clf = GridSearchCV(forest, train_params, scoring=scorer_, verbose=2, cv=4)
     clf.fit(data.values, y_true)
     return clf.best_estimator_
 
+
 def scorer(estimator, x_test, y_test):
+    """
+    Evaluation function
+    :param estimator: sklearn.model
+    :param x_test: pd.DataFrame
+        Dataset with predictors
+    :param y_test: pd.Series
+        Target values
+    :return: double
+        roc_auc_score
+    """
     y_score = estimator.decision_function(x_test)
     return roc_auc_score(y_test, y_score)
 
+
 def unknown_pref_metric(y_true, y_pred):
+    """
+    Recall calculation
+    :param y_true: pd.series
+        True target values
+    :param y_pred: pd.series
+        Predicted values target
+    :return: double
+        recall score
+    """
     correct_preds_r = sum(y_true & y_pred)
     trues = sum(y_true)
     return (correct_preds_r / trues) * 100
@@ -40,6 +82,12 @@ unknown_pref_scorer = make_scorer(unknown_pref_metric, greater_is_better=True)
 
 
 def get_stat_param_func(data):
+    """
+    Function for extracting attributes from dataframe
+    :param data: pd.DataFrame
+    :return: function
+        Returns a function that allows extraction from the feature column of the dataframe data param attribute
+    """
     @lru_cache
     def get_stat_param(feature, param):
         return getattr(data[feature], param)()
@@ -47,6 +95,11 @@ def get_stat_param_func(data):
 
 
 def generate_random_rows(data, count):
+    """
+    :param data: pd.DataFrame
+    :param count: int
+    :return: dict
+    """
     get_param = get_stat_param_func(data)
     rows = []
     for _ in range(count):
@@ -63,10 +116,23 @@ def generate_random_rows(data, count):
 
 
 def append_rows(data, rows):
+    """
+
+    :param data: pd.DataFrame
+    :param rows: dict
+    :return: pd.DataFrame
+    """
     return data.append(rows, ignore_index=True)
 
 
 def unknown_and_custom_loss(model, x_data, true_is_anomaly):
+    """
+
+    :param model: sklearn.model
+    :param x_data: pd.DataFrame
+    :param true_is_anomaly: pd.DataFrame
+    :return:
+    """
     scores = model.score_samples(x_data)
     scores_order = scores.argsort()
     len_for_check = 3000
@@ -78,7 +144,19 @@ def unknown_and_custom_loss(model, x_data, true_is_anomaly):
 
     return (found / len_for_check) * 100
 
+
 def fink_ad_model_train():
+    """
+    :return: None
+    The function saves 6 files in the call directory:
+        forest_g.onnx - Trained model for filter _g in the format onnx
+        forest_r.onnx - Trained model for filter _r in the format onnx
+        forest_g.pickle - Trained model for filter _g in the format pickle
+        forest_r.pickle - Trained model for filter _r in the format pickle
+        _g_means.csv - mean values for filter _g
+        _r_means.csv - mean values for filter _r
+
+    """
     parser = argparse.ArgumentParser(description='Fink AD model training')
     parser.add_argument('dataset_dir', type=str, help='Input dir for dataset')
     parser.add_argument('--n_jobs', type=int, default=-1, help='Number of threads (default: -1)')
@@ -88,14 +166,16 @@ def fink_ad_model_train():
     assert os.path.isfile(train_data_path), 'The specified training dataset file does not exist!'
     filter_base = ('_r', '_g')
     print('Loading training data...')
-    x = pd.read_parquet(train_data_path)
-    print(f'data shape: {x.shape}')
-    features_1 = x["lc_features"].apply(lambda data: extract_one(data, "1")).add_suffix("_r")
-    features_2 = x["lc_features"].apply(lambda data: extract_one(data, "2")).add_suffix("_g")
+    x_buf_data = pd.read_parquet(train_data_path)
+    print(f'data shape: {x_buf_data.shape}')
+    features_1 = x_buf_data["lc_features"].apply(lambda data:
+        extract_one(data, "1")).add_suffix("_r")
+    features_2 = x_buf_data["lc_features"].apply(lambda data:
+        extract_one(data, "2")).add_suffix("_g")
 
     print('Filtering...')
     data = pd.concat([
-    x.drop(['lc_features', 'cjd', 'cfid', 'cmag', 'cerrmag', 'is_null'], axis=1),
+    x_buf_data.drop(['lc_features', 'cjd', 'cfid', 'cmag', 'cerrmag', 'is_null'], axis=1),
     features_1,
     features_2,
     ], axis=1).dropna(axis=0)
@@ -103,18 +183,18 @@ def fink_ad_model_train():
     datasets = defaultdict(lambda: defaultdict(list))
 
     with tqdm(total=len(data)) as pbar:
-        for index, row in data.iterrows():
+        for _, row in data.iterrows():
             for passband in filter_base:
                 new_data = datasets[passband]
                 new_data['object_id'].append(row.objectId)
                 new_data['class'].append(row['class'])
-                for col, r in zip(data.columns, row):
+                for col, r_data in zip(data.columns, row):
                     if not col.endswith(passband):
                         continue
-                    new_data[col[:-2]].append(r)
+                    new_data[col[:-2]].append(r_data)
             pbar.update()
 
-    main_data = dict()
+    main_data = {}
     for passband in datasets:
         new_data = datasets[passband]
         new_df = pd.DataFrame(data=new_data)
@@ -137,25 +217,26 @@ def fink_ad_model_train():
         'weighted_mean',
         'mean'
     ]
-    data = {key : item.drop(labels=['object_id', 'class'] + common_rems, axis=1) for key, item in data.items()}
+    data = {key : item.drop(labels=['object_id', 'class'] + common_rems,
+                axis=1) for key, item in data.items()}
     for key, item in data.items():
         item.mean().to_csv(f'{key}_means.csv')
     print('Training...')
     for key in filter_base:
         is_unknown = classes[key] == 'Unknown'
         search_params_unknown = {
-        'n_estimators': (100, 150, 200, 300, 500),
-        'max_features':(0.5, 0.6, 0.7, 0.8, 0.9, 1.0),
-        'contamination': (sum(is_unknown) / len(data[key]),),
-        'bootstrap': (True,),
-        'max_samples': (0.5, 0.6, 0.7, 0.8, 0.9, 1.0),
-        'n_jobs': (n_jobs,)
+            'n_estimators': (100, 150, 200, 300, 500),
+            'max_features':(0.5, 0.6, 0.7, 0.8, 0.9, 1.0),
+            'contamination': (sum(is_unknown) / len(data[key]),),
+            'bootstrap': (True,),
+            'max_samples': (0.5, 0.6, 0.7, 0.8, 0.9, 1.0),
+            'n_jobs': (n_jobs,)
         }
         forest_simp = train_with_forest(
-        data[key],
-        search_params_unknown,
-        scorer,
-        is_unknown
+            data[key],
+            search_params_unknown,
+            scorer,
+            is_unknown
         )
         with open(f'forest{key}.pickle', 'wb') as handle:
             pickle.dump(forest_simp, handle)
