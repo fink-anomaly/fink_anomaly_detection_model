c1ec127e10033d7231c4fe722e2f55a4d1b2622b
Timofei Pshenichnyy <timofei.psheno@gmail.com>
2023-11-11 12:54:44 +0300

    added light_curve in req

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ def readme():
 
 
 setup(name='fink_anomaly_detection_model',
-      version='0.4.4',
+      version='0.4.5',
       description='Fink anomaly detection model',
       packages=find_packages(),
       author_email='timofei.psheno@gmail.com',
       install_requires=['scikit-learn>=1.3.1', 'numpy>=1.23.5',
       'tqdm>=4.65.0', 'onnx>=1.14.0', 'scipy>=1.10.1',
       'skl2onnx>=1.15.0', 'pyarrow>=13.0.0', 'coniferest>=0.0.11', 'telethon',
-      'slack_sdk', 'config', 'configparser', 'fink_science', 'pyspark==3.1.3'],
+      'slack_sdk', 'config', 'configparser', 'fink_science', 'pyspark==3.1.3', 'light_curve'],
       entry_points={
         'console_scripts': [
             'fink_ad_model_train = fink_anomaly_detection_model:fink_ad_model_train',
