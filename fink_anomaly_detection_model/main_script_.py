21f9f5f02e3ea5a829d4eb8fa8e0946b03ef91ac
Timofei Pshenichnyy <timofei.psheno@gmail.com>
2023-11-11 12:35:24 +0300

    fix?

--- fink_anomaly_detection_model/main_script_.py
+++ fink_anomaly_detection_model/main_script_.py
@@ -16,7 +16,7 @@ from coniferest.onnx import to_onnx as to_onnx_add
 from coniferest.aadforest import AADForest
 from sklearn.model_selection import train_test_split
 import itertools
-import reactions_reader
+import .reactions_reader
 
 
 def generate_param_comb(param_dict):
