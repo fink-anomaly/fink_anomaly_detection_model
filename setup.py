c3832ec1f9815c9a0aa1b1d0707a328ee2ea8b7f
Timofei Pshenichnyy <timofei.psheno@gmail.com>
2023-12-24 11:02:30 +0300

    file rename

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='fink_anomaly_detection_model',
-      version='0.4.24',
+      version='0.4.25',
       description='Fink SNAD Anomaly Detection Model',
       packages=find_packages(),
       author_email='timofei.psheno@gmail.com',
