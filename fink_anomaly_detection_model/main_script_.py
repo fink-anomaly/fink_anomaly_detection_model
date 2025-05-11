62ee7b555a519a48d5d2a5971ef5e5b790f1e75d
Knispel <timofei.psheno@gmail.com>
2025-05-11 10:43:22 +0300

    fix

--- fink_anomaly_detection_model/main_script_.py
+++ fink_anomaly_detection_model/main_script_.py
@@ -20,11 +20,11 @@ from coniferest.onnx import to_onnx as to_onnx_add
 from coniferest.aadforest import AADForest
 from sklearn.model_selection import train_test_split
 import itertools
-import reactions_reader as reactions_reader
+# import reactions_reader as reactions_reader
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-# import fink_anomaly_detection_model.reactions_reader as reactions_reader
+import fink_anomaly_detection_model.reactions_reader as reactions_reader
 
 
 FILTER_BASE = ('_r', '_g')
