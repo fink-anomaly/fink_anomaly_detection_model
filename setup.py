c544667db2044cc49a30fc324b036cb3e834d1a0
Knispel <timofei.psheno@gmail.com>
2025-06-29 11:30:07 +0300

    update deps

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ def readme():
 
 
 setup(name='fink_anomaly_detection_model',
-      version='0.4.64',
+      version='0.4.65',
       description='Fink SNAD Anomaly Detection Model',
       packages=find_packages(),
       author_email='timofei.psheno@gmail.com',
-      install_requires=['scikit-learn>=1.3.1', 'numpy>=1.23.5', 'pandas==2.0.2',
+      install_requires=['scikit-learn>=1.3.1', 'numpy==1.26.4', 'pandas==2.0.2',
       'tqdm>=4.65.0', 'onnx>=1.14.0', 'scipy>=1.10.1',
       'skl2onnx>=1.15.0', 'pyarrow>=13.0.0', 'coniferest<=0.0.14', 'telethon',
-      'slack_sdk', 'config', 'configparser', 'fink_science', 'pyspark==3.1.3', 'light_curve', 'psutil', 'seaborn==0.13.2', 'matplotlib==3.9.0', 'optuna', 'requests'],
+      'slack_sdk', 'config', 'configparser', 'fink_science==3.13.3', 'pyspark==3.1.3', 'light_curve', 'psutil', 'seaborn==0.13.2', 'matplotlib==3.9.0', 'optuna', 'requests'],
       entry_points={
         'console_scripts': [
             'fink_ad_model_train = fink_anomaly_detection_model:fink_ad_model_train',
